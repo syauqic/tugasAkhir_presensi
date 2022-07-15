bd0cfb8f5feeebe01303e60a4bb2a2602312724f
Syauqi Benaputra <syauqi.benaputra1201@gmail.com>
2022-07-15 20:43:05 +0700

    Creating model part 1

---
+++ app/Models/dosenModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class dosenModel extends Model
+{
+    use HasFactory;
+}
---
+++ app/Models/jadwalModel.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class jadwalModel extends Model
+{
+    use HasFactory;
+}
